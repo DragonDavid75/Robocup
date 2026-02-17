5e153616f14cd0745cdb1f5d3f6d9c8441ed4e22
D <bzsombi99@gmail.com>
2026-02-17 20:09:46 +0100

    time stamps for world model data

--- core/world_model.py
+++ core/world_model.py
@@ -6,6 +6,11 @@ class WorldModel:
     def __init__(self):
         self.lock = threading.Lock()
 
+        # --- Time stamps --- Always update these from the main threads and respective threads
+        self.last_update_time_motion = 0.0
+        self.last_update_time_sensors = 0.0
+        self.last_update_time_manager = 0.0
+
         # --- Robot state ---
         self.pose_x = 0.0
         self.pose_y = 0.0
