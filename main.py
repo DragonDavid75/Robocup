6c5837f10042db8270a2e597526068dc7d03b9c5
AijaMonika <monika.vainina@gmail.com>
2026-03-10 17:40:14 +0100

    Add track visualizer with real-time robot position overlay

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from core.motion_controller import MotionController
 from core.vision_system import VisionSystem
 from core.mission_manager import MissionManager
 from core.localizer import Localizer
+from core.visualizer import TrackVisualizer
 import time
 
 world = WorldModel()
@@ -12,11 +13,13 @@ robot = RobotInterface()
 motion = MotionController(world, robot)
 vision = VisionSystem(world)
 localizer = Localizer(world)
+visualizer = TrackVisualizer(world)
 mission = MissionManager(world, robot)
 
 motion.start()
 vision.start()
 localizer.start()
+visualizer.start()
 
 try:
     while True:
@@ -29,4 +32,5 @@ except KeyboardInterrupt:
 motion.stop()
 vision.stop()
 localizer.stop()
+visualizer.stop()
 robot.terminate()
