450eddb1e8f1b79f5da5d26f8e022f3034d85411
AijaMonika <monika.vainina@gmail.com>
2026-03-11 16:44:48 +0100

    Remove local visualizer, keep terminal output

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from core.motion_controller import MotionController
 from core.vision_system import VisionSystem
 from core.mission_manager import MissionManager
 from core.localizer import Localizer
-from core.visualizer import TrackVisualizer
 import time
 
 world = WorldModel()
@@ -13,13 +12,11 @@ robot = RobotInterface()
 motion = MotionController(world, robot)
 vision = VisionSystem(world)
 localizer = Localizer(world)
-visualizer = TrackVisualizer(world)
 mission = MissionManager(world, robot)
 
 motion.start()
 vision.start()
 localizer.start()
-visualizer.start()
 
 try:
     while True:
@@ -32,5 +29,4 @@ except KeyboardInterrupt:
 motion.stop()
 vision.stop()
 localizer.stop()
-visualizer.stop()
 robot.terminate()
