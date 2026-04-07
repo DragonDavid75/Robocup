ca0a1a417b949d941678adea2e3cb2196c78e047
nil172001 <matarollobetn@gmail.com>
2026-04-07 15:08:27 +0200

    first golf ball changes

--- tasks/advanced_tasks/first_golf.py
+++ tasks/advanced_tasks/first_golf.py
@@ -20,6 +20,7 @@ class Firstball(BaseTask):
         # STATE 0: Drive to the ball using the vision-based system
         if self.state == 1:
             if not self.motion_controller.is_busy():
+                self.servo_controller.servo_control(1, -800, 300)
                 print("[TASK] Ball reached. Now following line for 2.5m")
                 self.motion_controller.follow_for_distance(2.5, 0.6)
                 self.state = 2
@@ -29,7 +30,7 @@ class Firstball(BaseTask):
             if not self.motion_controller.is_busy():
                 print("[TASK] Line follow complete. Driving straight for 0.5m")
                 self.motion_controller.drive_distance(0.5, 0.6)
-                self.state = 3
+                self.state = 3  
 
         # STATE 3: Wait for drive distance (0.5m) to finish
         elif self.state == 3:
@@ -49,22 +50,27 @@ class Firstball(BaseTask):
         elif self.state == 5:
             if not self.servo_controller.is_busy():
                 self.servo_controller.servo_control(1, -800, 300)
-                self.state = 6
+                self.state = 6.5
         #state 6.5: turn around on your position 45 degrees
         elif self.state == 6.5:
             if not self.servo_controller.is_busy():
                 print("[TASK] Servos activated. Turning around...")
-                self.motion_controller.turn_in_place(0.785)  # Turn 45 degrees
-                self.state = 7
+                self.motion_controller.turn_in_place(-1.9)  # Turn 45 degrees
+                self.state = 7.5
 
-        # STATE 6: go to ball
-        elif self.state == 6:
+        elif self.state == 7.5:
+            if not self.motion_controller.is_busy():
+                print("[TASK] Turn complete. Driving to ball again...")
+                self.state = 8
+
+        # STATE 7: go to ball
+        elif self.state == 8:
             if not self.servo_controller.is_busy():
                 print("[TASK] Servos activated. Driving to ball again...")
                 self.motion_controller.drive_to_ball()
-                self.state = 7
+                self.state = 9
         
-        elif self.state == 7:
+        elif self.state == 9:
             if not self.servo_controller.is_busy():
                 print("[TASK] BallFollowAndDriveTask completed successfully")
                 return TaskStatus.DONE
