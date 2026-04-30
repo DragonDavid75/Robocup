3dbcbe21198be9f89c02fc32c7a66f12ed878944
AijaMonika <monika.vainina@gmail.com>
2026-04-30 10:42:59 +0200

    with the new batteries it never turns enough, so I added a bit more rotation

--- tasks/advanced_tasks/b_timer_first.py
+++ tasks/advanced_tasks/b_timer_first.py
@@ -35,7 +35,7 @@ class TimerFirstTask(BaseTask):
 
         elif self.state == 1:
             if not self.motion_controller.is_busy():
-                self.motion_controller.turn_in_place(-1.45)
+                self.motion_controller.turn_in_place(-1.35)
                 self.state = 2
 
         elif self.state == 2:
